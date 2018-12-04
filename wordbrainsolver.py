99281f31147097b440c77b963f1f406f536ef74a
manymuch <zjx219@gmail.com>
2018-12-04 18:09:49 -0500

    drop test in main

--- wordbrainsolver.py
+++ wordbrainsolver.py
@@ -55,28 +55,72 @@ def read_wordlist(filename):
     with open(filename) as file:
         return file.read().split()
 
+def dictionarize(puzzle):
+    #join the list member to a string and dictionarize the puzzle letters
+    letter = ''.join(puzzle)
+    dict = {}
+    for idx, char in enumerate(letter):
+        if char in dict:
+            dict[char] = dict[char]+1
+        else:
+            dict[char] = 1
+    return dict
+
+
+#finding possible word combnition from wordlist
+def W2L(word_dict,letter,number):
+    letter_dict = dictionarize(letter)
+    result = []
+    if number in word_dict:
+        #iterate through all the words that have length=number
+        for idx,word in enumerate(word_dict[number]):
+            #iterate through all the character in a word
+            letter_dict_copy = letter_dict.copy()
+            flag = True
+            for idx,char in enumerate(list(word)):
+                if char in letter_dict_copy:
+                    if letter_dict_copy[char] == 1:
+                        letter_dict_copy.pop(char)
+                    else:
+                        letter_dict_copy[char] = letter_dict_copy[char] - 1
+                else:
+                    flag = False
+                    break
+            if flag:
+                result.append(word)
+    return result
+
+
+
 if __name__== "__main__":
     small_dict = classify_length(read_wordlist(argv[1]))
 
 
-    # Currently this bunch only do one job and quit
-    letters_list = []
+    # Dead loop, only exit with EOFError
     while True:
-        letter = input()
-        if '*' in letter:
-            answer_format = letter
-            break
-        else:
-            letters_list.append(letter)
-
-
-    # test for drop
-    a_wordbox = wordbox(letters_list)
-    a_wordbox.show()
-    a_wordbox.drop()
-    print("   ")
-    a_wordbox.show()
-
-
-    
+        letters_list = []
+        answer_length = []
+        flag = False
+        # Loop for each puzzle
+        while True:
+            letter = input()
+            if '*' in letter:
+                answer_pattern = letter.split()
+                for pattern in answer_pattern:
+                    answer_length.append(pattern.count('*'))
+                flag = True
+                break
+            else:
+                if flag:
+                    break
+                else:
+                    letters_list.append(letter)
+        a_wordbox = wordbox(letters_list)
+        a_wordbox.show()
+        print(dictionarize(letters_list))
+        print(answer_length)
+        # Solve for each solution
+        for WordNumber in answer_length:
+            raw_result = W2L(small_dict, letters_list, WordNumber)
+            print(raw_result)
     # ---------------------quit---------------------
