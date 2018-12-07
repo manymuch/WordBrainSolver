7fa591df613148b5ae49726ba3e38c8dd5a75b99
manymuch <zjx219@gmail.com>
2018-12-06 22:08:15 -0500

    tai nan le

--- wordbrainsolver.py
+++ wordbrainsolver.py
@@ -6,63 +6,6 @@ import numpy as np
 from collections import Counter
 from sys import argv
 
-class wordbox():
-    """create a NxN box for box"""
-    def __init__(self,letters_list):
-        self.length = len(letters_list[0])+2
-        self.letters_array = [[0]*self.length]*self.length
-        for i in range(1, self.length-1):
-            self.letters_array[i] = [0]+list(letters_list[i-1])+[0]
-        self.letters_array = np.asarray(self.letters_array)
-
-    def __drop_column(self, column):
-        """drop in a column"""
-        for i in range(1, self.length-2):
-            if column[-i] == '0':
-                # look up and find a non zero element
-                j = i + 1
-                while (column[-j] == '0'):
-                    if j >= self.length-2:
-                        return column
-                    j = j + 1
-                # switch that non zero elment with the zero element with index=i
-                column[-i] = column[-j]
-                column[-j] = '0'
-        return column
-
-    def drop(self):
-        """do something that drop the letter if there are 0 in the box"""
-        for column_idx in range(1,self.length):
-            column = self.letters_array[1:-1,column_idx]
-            self.letters_array[1:-1,column_idx] = self.__drop_column(column)
-
-    def show(self):
-        for letters in self.letters_array:
-            print(letters)
-
-    # def find_letter(self,x,y,letter):
-    #     threebythree = self.letters_array[x-1:x+2,y-1:y+2]
-    #     threebythree[1,1] = '0'
-    #     possible_answer = np.transpose(np.where(threebythree == letter))
-    #     for coordinate in possible_answer:
-    #         yield threebythree[coordinate[0]][coordinate[1]], coordinate
-    #
-    # def find_next(self,x,y):
-    #
-    #
-    #
-    #
-    # def find_path(self,word):
-    #     for letter in list(word):
-    #         next = self.find_letter(x,y,letter)
-    #         if next is not None:
-
-
-
-
-
-
-
 def classify_length(word_list):
     """preprocess the word list to dictionary of list, copy from Asignment8"""
     whole_dict = {}
@@ -80,16 +23,10 @@ def read_wordlist(filename):
 
 def input_puzzle(letters_list):
     letter = input()
-    if '*' in letter:
-        answer_length = []
-        answer_pattern = letter.split()
-        for pattern in answer_pattern:
-            answer_length.append(pattern.count('*'))
-        letters_list.append(answer_length)
-    else:
-        letters_list.append(letter)
+    letters_list.append(letter)
+    if '*' not in letter:
         input_puzzle(letters_list)
-        return letters_list
+    return letters_list
 
 def dictionarize(puzzle):
     #join the list member to a string and dictionarize the puzzle letters
@@ -105,30 +42,47 @@ def dictionarize(puzzle):
 
 
 #------word list tree------------------------
-class Node:
-    # to be complete
-    def __init__(self, data):
-        self.data = data
-        self.children = {}
+class Trie():
 
-    def add_child(self, data):
-        self.children[data] = {}
+    def __init__(self,word_list):
+        self._end = '*'
+        self.trie = dict()
+        for word in word_list:
+            self.add_word(word)
 
-class Trie:
-    # to be complete
-    def __init__(self):
-        self.is_word = False
-        self.child = {}
+    def __repr__(self):
+        return repr(self.trie)
 
-    def add(self, word):
-        if word[0] in self.head:
-            self.add
+    def add_word(self, word):
+
+        temp_trie = self.trie
+        for letter in word:
+            if letter in temp_trie:
+                temp_trie = temp_trie[letter]
+            else:
+                temp_trie = temp_trie.setdefault(letter, {})
+        temp_trie[self._end] = self._end
+        return temp_trie
+
+    def find_next(self, word,next_letter):
+        sub_trie = self.trie
+        for letter in word:
+            sub_trie = sub_trie[letter]
+        return (next_letter in sub_trie)
+
+    def find_word(self, word):
+        sub_trie = self.trie
+
+        for letter in word:
+            if letter in sub_trie:
+                sub_trie = sub_trie[letter]
+            else:
+                return False
         else:
-            self.head[word[0]] = {}
-            self.add(word[1:])
-
-
-
+            if self._end in sub_trie:
+                return True
+            else:
+                return False
 
 
 
@@ -140,8 +94,8 @@ class NoNegativeList(list):
             raise IndexError("...")
         return list.__getitem__(self, n)
 
-class solve_all():
-    def __init__(self,letters_list):
+class wordbrainsolver():
+    def __init__(self,letters_list,trie):
         # the grid here and index is like this
         # if you input:
         # abc
@@ -151,8 +105,8 @@ class solve_all():
         # the grid is
         # ['g','d','a'],['h','e','b'],['i','f','c']
         #
-        self.route = []
-        self.index = 0 # answer_line index
+        self.trie = trie
+        #self.index = 0 # answer_line index
         self.length = len(letters_list)
         self.grid = NoNegativeList([])
         for i in range(self.length):
@@ -162,14 +116,19 @@ class solve_all():
             self.grid.append(column)
         print(self.grid)
 
+    def all_grid(self):
+        for x,column in enumerate(self.grid):
+            for y,letter in enumerate(column):
+                yield letter, [x,y]
+
     # get all surround letter for a specific coordinate
-    def surround(self,coordinate):
+    def surround(self,coordinate,route):
         x = coordinate[0]
         y = coordinate[1]
-        print(self.grid[x][y])
+        # print(self.grid[x][y])
         for i in range(x-1,x+2):
             for j in range(y-1,y+2):
-                if i!=x or j!=y:
+                if [i,j] not in route:
                     try:
                         yield self.grid[i][j], [i,j]
                     except:
@@ -177,57 +136,49 @@ class solve_all():
 
     # answer_line shoud be a list
     # for example answer_line = list("a**** *** ***")
-    def solve(self,answer_line,coordinate):
-        if answer_line[self.index] == ' ':
-            drop()
+    def solve(self, answer_line, coordinate, index,route):
+        # print(answer_line)
+        if answer_line[index] == ' ':# to be implemented
+            print("new word")
             # delete (all the letter before index) in self.grid according to route
-            self.index += 1
+            self.solve(answer_line, None, index+1)
+        elif len(answer_line) == index+1:
+            for letter, coordinate in self.surround(coordinate,route):
+                #print(str(answer_line[:index])+letter)
+                if self.trie.find_word(''.join(answer_line[:index])+letter):
+                    #print("find a word")
+                    local_route  = route.copy()
+                    local_route.append(coordinate)
+                    answer_line[index] = letter
+                    print(answer_line)
+            #print(' ')
+
         else:
-            for letter, coordinate in self.surround(coordinate):
-                route_copy = self.route.copy()
-                if answer_line[:self.index].has_a_child(letter):# has_a_child() to be implemented
-                    route_copy.append(coordinate)
-                    answer_line[self.index] = letter
-                    self.index += 1
-                    solve(answer_line, coordinate)
-
-
-
-#finding possible word combnition from wordlist
-def W2L(word_dict,letter,number):
-    letter_dict = dictionarize(letter)
-    result = []
-    if number in word_dict:
-        #iterate through all the words that have length=number
-        for idx,word in enumerate(word_dict[number]):
-            #iterate through all the character in a word
-            letter_dict_copy = letter_dict.copy()
-            flag = True
-            for idx,char in enumerate(list(word)):
-                if char in letter_dict_copy:
-                    if letter_dict_copy[char] == 1:
-                        letter_dict_copy.pop(char)
-                    else:
-                        letter_dict_copy[char] = letter_dict_copy[char] - 1
-                else:
-                    flag = False
-                    break
-            if flag:
-                result.append(word)
-    return result
-
-
+            if coordinate is None:
+                for letter, coordinate in self.all_grid():
+                    if self.trie.find_next(answer_line[:index],letter):
+                        local_route  = route.copy()
+                        local_route.append(coordinate)
+                        answer_line[index] = letter
+                        self.solve(answer_line, coordinate, index+1,local_route)
+            else:
+                for letter, coordinate in self.surround(coordinate,route):
+                    if self.trie.find_next(answer_line[:index],letter):
+                        local_route  = route.copy()
+                        local_route.append(coordinate)
+                        answer_line[index] = letter
+                        self.solve(answer_line, coordinate, index+1,local_route)
 
 if __name__== "__main__":
-    small_dict = read_wordlist(argv[1])
-
+    small_list = read_wordlist(argv[1])
+    small_trie = Trie(small_list)
     # Dead loop, only exit with EOFError
     while True:
         # Loop for each puzzle
-        whole_list = input_puzzle([])
-        letters_list = whole_list[:-1]
-        answer_length = whole_list[-1]
-
+        puzzle = input_puzzle([])
+        wordbrain = wordbrainsolver(puzzle[:-1],small_trie)
+        wordbrain.solve(list(puzzle[-1]),None,0,[])
+        exit()
 
 
 
